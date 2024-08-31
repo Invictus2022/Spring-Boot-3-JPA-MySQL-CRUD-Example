5d5ac0221f21fe55362a026cab3042e188256395
hritik <rohan.kinshu@gmail.com>
2024-08-31 16:23:03 +0530

    ..

---
+++ Spring-jpa/src/test/java/com/Spring_jpa/Spring_jpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_jpa.Spring_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
